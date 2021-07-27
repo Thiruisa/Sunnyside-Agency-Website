78f5862655291574abef62200825dde5b0971946
Thiruisa <45204969+Thiruisa@users.noreply.github.com>
2021-07-27 19:31:08 +0100

    Completed

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+}
